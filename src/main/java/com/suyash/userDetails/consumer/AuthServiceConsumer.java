a7e0cbf61d5c2442622540451623a23b0b0e6db7
Suyash Sharma <sharmasuyash198@gmail.com>
2026-05-31 22:11:17 +0530

    few changes in app properoties

--- src/main/java/com/suyash/userDetails/consumer/AuthServiceConsumer.java
+++ src/main/java/com/suyash/userDetails/consumer/AuthServiceConsumer.java
@@ -1,5 +1,26 @@
 package com.suyash.userDetails.consumer;
 
+import com.suyash.userDetails.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+
 public class AuthServiceConsumer {
+
+    UserRepository userRepository;
+
+    @Autowired
+    AuthServiceConsumer(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @KafkaListener( topics = "${kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void listen(Object eventData){
+        try{
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
