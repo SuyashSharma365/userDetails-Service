26f2a8006eb6b24ab982db3f9d0113f0b9cdc07a
Suyash Sharma <sharmasuyash198@gmail.com>
2026-06-01 23:02:02 +0530

    few changes kafka is working

--- src/main/java/com/suyash/userDetails/consumer/AuthServiceConsumer.java
+++ src/main/java/com/suyash/userDetails/consumer/AuthServiceConsumer.java
@@ -1,9 +1,13 @@
 package com.suyash.userDetails.consumer;
 
 import com.suyash.userDetails.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceConsumer {
 
     UserRepository userRepository;
@@ -16,7 +20,7 @@ public class AuthServiceConsumer {
     @KafkaListener( topics = "${kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void listen(Object eventData){
         try{
-
+            System.out.println(eventData);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/suyash/userDetails/entities/UserInfo.java
+++ src/main/java/com/suyash/userDetails/entities/UserInfo.java
@@ -2,6 +2,7 @@ package com.suyash.userDetails.entities;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.persistence.Entity;
@@ -23,13 +24,18 @@ public class UserInfo {
 
     private Long id;
     @Id
+    @JsonProperty("user_id")
     private String userId;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
     private String lastName;
 
+    @JsonProperty("phone_number")
     private Long phoneNumber;
 
+    @JsonProperty("email")
     private String email;
 }
