4c77bcd73c1c9e4241a7c938239b88965aa2273a
Suyash Sharma <sharmasuyash198@gmail.com>
2026-06-08 01:05:05 +0530

    commented the new test cases

--- src/test/java/com/suyash/userDetails/UserDetailsApplicationTests.java
+++ src/test/java/com/suyash/userDetails/UserDetailsApplicationTests.java
@@ -3,10 +3,10 @@ package com.suyash.userDetails;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class UserDetailsApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
